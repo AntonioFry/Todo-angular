650e43ff0c81196a4e25be67e5e7d8bd02a42342
Antonio Fry <antoniofry2000@gmail.com>
2020-02-25 13:05:52 -0700

    Create dynamic classes functionality

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -14,4 +14,12 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
 }
